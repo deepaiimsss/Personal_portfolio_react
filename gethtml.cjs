554bd653cd9c52d04ba18ffded61a2dfc505a735
Deepjit Das <dasdeepjit1234@gmail.com>
2026-08-20 09:57:29 +0530

    Fix About section mobile layout: reduce left/right padding, remove overflow clipping, and optimize typography scaling for 390px screens

--- gethtml.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/htmlContent.js', 'utf8');
-const start = content.indexOf('<section id="about"');
-const end = content.indexOf('</section>', start);
-console.log(content.substring(start, end + 10));
